b0edca446d76b5d8936c375b31feca5edeff5c21
Carlos <carloshalbuquerque2@gmail.com>
2021-05-14 08:55:22 -0300

    part 2 finished

--- jobsCalc/src/server.js
+++ jobsCalc/src/server.js
@@ -6,12 +6,15 @@ const routes = require("./routes")
 
 // usando template engine
 server.set('view engine', 'ejs')
-
+ 
 // middleware, habilitar arquivos statics - criando uma rota para cada elemento do public
-server.use(express.static("public"))
+server.use(express.static("public")) 
+
+//liberação para usar o req.body em uma rquisição
+server.use(express.urlencoded({ extended: true }))
 
 //routes 
 server.use(routes)
 
 // listen é uma functionalidade que vai ligar nosso server 
-server.listen(3000, () => console.log("rodando"))
+server.listen(3001, () => console.log("rodando"))
